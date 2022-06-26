1c2d1a58f01591672b734bc8e3197fb56d61d160
blendthink <info@blendthink.dev>
2022-06-26 14:58:40 +0900

    🚀 remove unnecessary judgments

--- tool/grind.dart
+++ tool/grind.dart
@@ -32,17 +32,15 @@ updateDependencyLints() {
 
   final packagesDir = Directory('$pubCachePath/hosted/pub.dartlang.org');
   packagesDir.listSync().forEach((e) {
-    if (e is Directory) {
-      final dirPath = e.path;
-      if (dirPath.contains(RegExp(r'.+/flutter_lints-.+'))) {
-        File('$dirPath/lib/flutter.yaml')
-            .copySync('tool/lints/flutter_lints/flutter.yaml');
-      }
-      if (dirPath.contains(RegExp(r'.+/lints-.+'))) {
-        File('$dirPath/lib/core.yaml').copySync('tool/lints/lints/core.yaml');
-        File('$dirPath/lib/recommended.yaml')
-            .copySync('tool/lints/lints/recommended.yaml');
-      }
+    final dirPath = e.path;
+    if (dirPath.contains(RegExp(r'.+/flutter_lints-.+'))) {
+      File('$dirPath/lib/flutter.yaml')
+          .copySync('tool/lints/flutter_lints/flutter.yaml');
+    }
+    if (dirPath.contains(RegExp(r'.+/lints-.+'))) {
+      File('$dirPath/lib/core.yaml').copySync('tool/lints/lints/core.yaml');
+      File('$dirPath/lib/recommended.yaml')
+          .copySync('tool/lints/lints/recommended.yaml');
     }
   });
 }
